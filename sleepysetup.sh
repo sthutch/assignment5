6bfac4c574ec3d8440dbf03cbe78674c72629fba
shutchings <steve@dazoo.org>
2015-04-15 15:31:56 -0600

    new repo from failed old one

---
+++ sleepysetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Set's permissions of modules
+
+sudo chmod 777 /dev/sleepy0
+sudo chmod 777 /dev/sleepy1
+sudo chmod 777 /dev/sleepy2
+sudo chmod 777 /dev/sleepy3
+sudo chmod 777 /dev/sleepy4
+sudo chmod 777 /dev/sleepy5
+sudo chmod 777 /dev/sleepy6
+sudo chmod 777 /dev/sleepy7
+sudo chmod 777 /dev/sleepy8
+sudo chmod 777 /dev/sleepy9
